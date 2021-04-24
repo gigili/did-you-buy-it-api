6e263143521482c9a0a615f3e806e9cc924ad5f4
Igor Ilic <github@igorilic.net>
2021-04-24 22:27:19 +0200

    Fix duplicate code in Logger

--- utility/classes/Logger.php
+++ utility/classes/Logger.php
@@ -4,64 +4,41 @@
 
 	class Logger
 	{
-		public static function log(string $message = "")
+		public static function log(string $message = "", string $logFileName = "app_log.log")
 		{
 			if ( is_dir($_SERVER["DOCUMENT_ROOT"] . "/logs") === false ) {
 				mkdir($_SERVER["DOCUMENT_ROOT"] . "/logs", 0644);
 			}
 
-			$prefix = "[" . date("Y-m-d H:i:s") . "] ";
-
-			$handle = fopen($_SERVER["DOCUMENT_ROOT"] . "/logs/log.txt", "a+");
-			fwrite($handle, $prefix . $message . "\n");
+			$handle = fopen($_SERVER["DOCUMENT_ROOT"] . "/logs/{$logFileName}", "a+");
+			fwrite($handle, $message . "\n");
 			fwrite($handle, $message . "\n");
 			fclose($handle);
 		}
 
-		public static function warning(string $message = "")
+		public static function warning(string $message = "", string $logFileName = "app_log.log")
 		{
-			if ( is_dir($_SERVER["DOCUMENT_ROOT"] . "/logs") === false ) {
-				mkdir($_SERVER["DOCUMENT_ROOT"] . "/logs", 0644);
-			}
-
 			$prefix = "[" . date("Y-m-d H:i:s") . " | WARNING] ";
-
-			$handle = fopen($_SERVER["DOCUMENT_ROOT"] . "/logs/log.txt", "a+");
-			fwrite($handle, $prefix . $message . "\n");
-			fwrite($handle, $message . "\n");
-			fclose($handle);
+			self::log($prefix . $message, $logFileName);
 		}
 
-		public static function error(string $message = "")
+		public static function error(string $message = "", string $logFileName = "app_log.log")
 		{
-			if ( is_dir($_SERVER["DOCUMENT_ROOT"] . "/logs") === false ) {
-				mkdir($_SERVER["DOCUMENT_ROOT"] . "/logs", 0644);
-			}
-
-			$prefix = "[" . date("Y-m-d H:i:s") . " | ERROR] ";
-
-			$handle = fopen($_SERVER["DOCUMENT_ROOT"] . "/logs/log.txt", "a+");
-			fwrite($handle, $prefix . $message . "\n");
-			fclose($handle);
+			$prefix = '[' . date('Y-m-d H:i:s') . ' | ERROR] ';
+			self::log($prefix . $message, $logFileName);
 		}
 
-		public static function log_network_request(string $message, array|object|null $response = NULL)
+		public static function log_network_request(string $message, array|object|null $response = NULL, string $logFileName = "app_log.log")
 		{
-			if ( is_dir($_SERVER["DOCUMENT_ROOT"] . "/logs") === false ) {
-				mkdir($_SERVER["DOCUMENT_ROOT"] . "/logs", 0644);
-			}
-
-			$handle = fopen($_SERVER["DOCUMENT_ROOT"] . "/logs/log.txt", "a+");
-			fwrite($handle, "=====================================================\n");
-			fwrite($handle, "Date: " . date("Y-m-d H:i:s") . "\n");
-			fwrite($handle, "IP: {$_SERVER["REMOTE_ADDR"]}\n");
-			fwrite($handle, "Request method: {$_SERVER["REQUEST_METHOD"]}\n");
-			fwrite($handle, "Request params: \n" . json_encode($_REQUEST) . "\n");
+			self::log("=====================================================\n", $logFileName);
+			self::log("Date: " . date("Y-m-d H:i:s") . "\n", $logFileName);
+			self::log("IP: {$_SERVER["REMOTE_ADDR"]}\n", $logFileName);
+			self::log("Request method: {$_SERVER["REQUEST_METHOD"]}\n", $logFileName);
+			self::log("Request params: \n" . json_encode($_REQUEST) . "\n", $logFileName);
 			if ( !is_null($response) ) {
-				fwrite($handle, "Response: \n" . json_encode($response) . "\n");
+				self::log("Response: \n" . json_encode($response) . "\n", $logFileName);
 			}
-			fwrite($handle, $message);
-			fwrite($handle, "=====================================================\n");
-			fclose($handle);
+			self::log($message, $logFileName);
+			self::log("=====================================================\n", $logFileName);
 		}
 	}
